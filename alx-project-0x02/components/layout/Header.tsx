d79edafc348f42927ae449dafd619bfab516d211
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-19 17:12:54 +0100

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,21 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4 flex gap-6">
+      <h1 className="font-semibold">ALX Project — Next.js Setup</h1>
+
+      <nav className="ml-auto flex gap-4">
+        <Link href="/home" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
 
