156ba9eb4f7956ac7aa610ddedb6d364cc414dfc
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-19 17:11:36 +0100

    Update home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,1 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">Home Page</h1>
+      <p className="mt-2">Welcome to the Home page.</p>
+    </main>
+  );
+}
 
