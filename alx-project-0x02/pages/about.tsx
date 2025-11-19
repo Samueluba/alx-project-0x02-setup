6cc710404ed06cc346d53e92ae6dd4738baff69a
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-11-19 17:12:03 +0100

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,1 +1,9 @@
+export default function AboutPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">About Page</h1>
+      <p className="mt-2">Learn more about us on this page.</p>
+    </main>
+  );
+}
 
